9172a54a6dd39b9d223f85b8f6ae86500fb3350f
Danielovich <danielfrostdk@outlook.com>
2024-08-11 16:02:03 +0200

    integration tests

---
+++ logparserkata/CommonPathDeterminatorIntegrationTests.cs
@@ -0,0 +1,21 @@
+﻿namespace logparserkata;
+
+public class CommonPathDeterminatorIntegrationTests
+{
+    [Fact]
+    public async Task Find_Most_Common_Three_Page_Path_Pattern_From_Loaded_File()
+    {
+        //Arrange
+        var logLoader = new LogFileLoader();
+        var logEntries = await logLoader.LoadEntriesAsync();
+
+        var sut = new CommonPathDeterminator(logEntries);
+
+        //Act
+        var pattern = sut.ThreePagePattern(sut.CommonPathPartitionsByUserId());
+        var oo = pattern.OrderByDescending(o => o.OccurenceCount).ToList();
+        //Assert
+
+    }
+
+}
